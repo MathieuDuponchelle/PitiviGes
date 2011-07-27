ee80caf7495b1d0840e203f2c0193956ba8db735
Mathieu Duponchelle <seeed@laposte.net>
2011-07-27 02:02:20 +0200

    ges: add track objects to the track before the track-object-added is emitted
    NOTE: The caller of ges_timeline_object_create_track_object now needs to add it to
    the timeline_object after calling the function.
    
    Fix the testsuite to support that accordingly

--- ges/ges-timeline-object.c
+++ ges/ges-timeline-object.c
@@ -379,7 +379,6 @@ ges_timeline_object_create_track_object (GESTimelineObject * object,
   }
 
   res = class->create_track_object (object, track);
-  ges_timeline_object_add_track_object (object, res);
   return res;
 
 }
@@ -421,13 +420,17 @@ ges_timeline_object_create_track_objects_func (GESTimelineObject * object,
     GESTrack * track)
 {
   GESTrackObject *result;
+  gboolean ret;
 
   result = ges_timeline_object_create_track_object (object, track);
   if (!result) {
     GST_WARNING ("couldn't create track object");
     return FALSE;
   }
-  return ges_track_add_object (track, result);
+  ges_track_object_set_timeline_object (result, object);
+  ret = ges_track_add_object (track, result);
+  ges_timeline_object_add_track_object (object, result);
+  return ret;
 }
 
 /**
@@ -461,6 +464,7 @@ ges_timeline_object_add_track_object (GESTimelineObject * object, GESTrackObject
     return FALSE;
 
   ges_track_object_set_timeline_object (trobj, object);
+
   g_object_ref (trobj);
 
   mapping = g_slice_new0 (ObjectMapping);
