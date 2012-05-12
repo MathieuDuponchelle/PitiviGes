05492d84acb0bb177f6aaf8d25270da1582c47f3
Volodymyr Rudyi <vladimir.rudoy@gmail.com>
2012-05-12 11:56:55 +0300

    tests: Test file skeleton for GESMaterial

---
+++ tests/check/ges/material.c
@@ -0,0 +1,65 @@
+/* GStreamer Editing Services
+ * Copyright (C) 2012 Volodymyr Rudyi <vladimir.rudoy@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <ges/ges.h>
+#include <gst/check/gstcheck.h>
+
+GST_START_TEST (test_list_standard_materials)
+{
+
+}
+
+GST_END_TEST;
+
+GST_START_TEST (test_get_track_types)
+{
+}
+
+GST_END_TEST;
+
+static Suite *
+ges_suite (void)
+{
+  Suite *s = suite_create ("ges");
+  TCase *tc_chain = tcase_create ("material");
+
+  suite_add_tcase (s, tc_chain);
+
+  tcase_add_test (tc_chain, test_list_standard_materials);
+  tcase_add_test (tc_chain, test_get_track_types);
+
+  return s;
+}
+
+int
+main (int argc, char **argv)
+{
+  int nf;
+
+  Suite *s = ges_suite ();
+  SRunner *sr = srunner_create (s);
+
+  gst_check_init (&argc, &argv);
+
+  srunner_run_all (sr, CK_NORMAL);
+  nf = srunner_ntests_failed (sr);
+  srunner_free (sr);
+
+  return nf;
+}
