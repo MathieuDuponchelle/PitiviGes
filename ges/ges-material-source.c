8c8bd9299e0877ebe3fba7443ac776296470e3e0
Volodymyr Rudyi <vladimir.rudoy@gmail.com>
2012-06-08 12:26:19 +0300

    material: Added URI property to source material

--- ges/ges-material-source.c
+++ ges/ges-material-source.c
@@ -22,6 +22,16 @@
 
 G_DEFINE_TYPE (GESMaterialSource, ges_material_source, GES_TYPE_MATERIAL);
 
+enum
+{
+  PROP_0,
+  PROP_URI,
+  PROP_LAST
+};
+
+static GParamSpec *properties[PROP_LAST];
+
+
 struct _GESMaterialSourcePrivate
 {
   gchar *uri;
@@ -30,9 +40,54 @@ struct _GESMaterialSourcePrivate
 };
 
 static void
+ges_material_source_get_property (GObject * object, guint property_id,
+    GValue * value, GParamSpec * pspec)
+{
+  GESMaterialSource *material = GES_MATERIAL_SOURCE (object);
+
+  switch (property_id) {
+    case PROP_URI:
+      g_value_set_string (value, material->priv->uri);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
+ges_material_source_set_property (GObject * object, guint property_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  GESMaterialSource *material = GES_MATERIAL_SOURCE (object);
+
+  switch (property_id) {
+    case PROP_URI:
+      if (material->priv->uri) {
+        g_free (material->priv->uri);
+      }
+
+      material->priv->uri = g_strdup (g_value_get_string (value));
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
 ges_material_source_class_init (GESMaterialSourceClass * klass)
 {
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
   g_type_class_add_private (klass, sizeof (GESMaterialSourcePrivate));
+
+  object_class->get_property = ges_material_source_get_property;
+  object_class->set_property = ges_material_source_set_property;
+  properties[PROP_URI] =
+      g_param_spec_string ("uri",
+      "URI of source material",
+      "Get/set URI of source material",
+      NULL, G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+
+  g_object_class_install_properties (object_class, PROP_LAST, properties);
 }
 
 static void
