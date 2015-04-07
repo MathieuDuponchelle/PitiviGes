3bde089765a272b8d3183db26a4ec17a472c1c49
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-04-07 21:27:21 +0200

    examples: improve play_timeline

--- examples/c/play_timeline.c
+++ examples/c/play_timeline.c
@@ -1,12 +1,29 @@
 #include "examples.h"
 
+static void
+bus_message_cb (GstBus * bus, GstMessage * message, GMainLoop * loop)
+{
+  switch (GST_MESSAGE_TYPE (message)) {
+    case GST_MESSAGE_EOS:
+      g_main_loop_quit (loop);
+      break;
+    default:
+      break;
+  }
+}
+
 void
 play_timeline (GESTimeline * timeline)
 {
   GMainLoop *loop = g_main_loop_new (NULL, FALSE);
   GESPipeline *pipeline = ges_pipeline_new ();
+  GstBus *bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+
+  gst_bus_add_signal_watch (bus);
+  g_signal_connect (bus, "message", G_CALLBACK (bus_message_cb), loop);
 
   ges_pipeline_set_timeline (pipeline, timeline);
   gst_element_set_state (GST_ELEMENT (pipeline), GST_STATE_PLAYING);
   g_main_loop_run (loop);
+  gst_element_set_state (GST_ELEMENT (pipeline), GST_STATE_NULL);
 }
--- examples/python/play_timeline.py
+++ examples/python/play_timeline.py
@@ -1,8 +1,18 @@
 from gi.repository import GES, Gst, GLib
 
+def bus_message_cb (bus, message, loop):
+    if message.type == Gst.MessageType.EOS:
+        loop.quit()
+
 def play_timeline(timeline):
     pipeline = GES.Pipeline.new()
+    loop = GLib.MainLoop()
+
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect ("message", bus_message_cb, loop)
+
     pipeline.set_timeline (timeline)
     pipeline.set_state (Gst.State.PLAYING)
-    loop = GLib.MainLoop()
     loop.run()
+    pipeline.set_state (Gst.State.NULL)
